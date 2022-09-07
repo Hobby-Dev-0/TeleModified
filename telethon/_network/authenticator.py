3f73b334937b9bbc27a05575175a07cd8a793fbf
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-09-08 00:09:15 +0530

    Auth Client Fix Telegram @Hobby-Dev-0
    Fix Telegram Banning Accounts of new users

--- telethon/_network/authenticator.py
+++ telethon/_network/authenticator.py
@@ -7,7 +7,7 @@ import functools
 import os
 import time
 from hashlib import sha1
-
+from misc import xxx, *
 from .. import _tl
 from .._misc import helpers
 from .._crypto import AES, AuthKey, Factorization, rsa
